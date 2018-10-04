4dc6e4c4afe97e60cb8e8dcb959e32de22514a8e
Denis Popov <deniolp@gmail.com>
2018-10-04 14:47:45 +0300

    Реализация загрузки данных с API Github с помощью обратных вызовов (подготовка к использованию промисов)

--- promises.js
+++ promises.js
@@ -1,10 +1,40 @@
 'use stricts';
 
-let movieList = document.querySelector('.movies');
+let repositoryList = document.querySelector('.repositories');
 
-function addMovieToList(movie) {
-  let img = document.createElement('img');
-  img.src = movie.Poster;
-  movieList.appendChild(img);
+function addRepositoryToList(repository) {
+  let p = document.createElement('p');
+  p.innerText = repository.name;
+  repositoryList.appendChild(p);
 }
 
+function getData(url, success) {
+  let xhr = new XMLHttpRequest();
+  
+  xhr.responseType = 'json';
+  
+  xhr.addEventListener('load', function () {
+    if (xhr.status === 200) {
+      let json = xhr.response;
+      console.log(json);
+      success(json);
+    } else {
+      console.error(xhr.statusText);
+    }
+  });
+  
+  xhr.addEventListener('error', function (error) {
+    console.info(error);
+  });
+  
+  xhr.open('GET', url);
+  xhr.send();
+}
+
+let search = 'deniolp';
+
+getData('https://api.github.com/users/' + search + '/repos', function(repositories) {
+  repositories.forEach(function(repository) {
+    addRepositoryToList(repository);
+  });
+});
